c1ae4e6c6f27d8ec1567a56d8e1c2f02214f6013
Tamanna Rana <tamanna.rana0917@gmail.com>
2026-07-08 19:01:48 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import com.chitkara.GenericList;
+
+public class Main{
+    public static void main(String[] args) {
+        var names=new GenericList<String>();
+        var numbers=new GenericList<Integer>();
+      //  var users=new GenericList<User>();
+
+        names.add("Tanu");
+        numbers.add(100);
+      //  users.add(new User());
+
+    }
+}
